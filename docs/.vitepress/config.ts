c0bcd47db54752ec00374ac277fbe06145ba9424
kristjanjansen <kristjan.jansen@gmail.com>
2021-12-21 18:10:43 +0200

    feat: config

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,3 +1,39 @@
-import { defineConfig } from "vitepress";
+import { createMarkdownRenderer, defineConfig } from "vitepress";
 
-export default defineConfig({ vue: { reactivityTransform: true } });
+function VisualiaMarkdownIt(md) {
+  const defaultFence = md.renderer.rules.fence;
+  const mdRenderer = createMarkdownRenderer("");
+  md.renderer.rules.fence = function () {
+    const [tokens, idx, _options, _env, _slf] = arguments;
+    const info = tokens[idx].info.trim();
+    if (info === "md") {
+      const { html } = mdRenderer.render(tokens[idx].content || "");
+      return `
+        ${defaultFence(...arguments)}
+        ${html}
+      `;
+    }
+    return defaultFence(...arguments);
+  };
+
+  const defaultHtmlBlock = md.renderer.rules.html_block;
+
+  md.renderer.rules.html_block = function () {
+    const [tokens, idx, _options, _env, _slf] = arguments;
+
+    return String(tokens[idx].content).startsWith("<v-")
+      ? `<p>${defaultHtmlBlock(...arguments)}</p>`
+      : defaultHtmlBlock(...arguments);
+  };
+
+  return md;
+}
+
+export default defineConfig({
+  vue: { reactivityTransform: true },
+  markdown: {
+    //@ts-ignore
+    breaks: true,
+    config: (md) => md.use(VisualiaMarkdownIt),
+  },
+});
