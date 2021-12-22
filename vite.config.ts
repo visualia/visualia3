22ad676bf458f5d85aa8a4387cfb5eda2b527663
kristjanjansen <kristjan.jansen@gmail.com>
2021-12-22 09:52:02 +0200

    feat: build split

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,55 @@
-import { defineConfig } from "vite";
+import { BuildOptions, defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import path from "path";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue({ reactivityTransform: true })],
+const build: Record<string, BuildOptions> = {
+  production: null,
+  es: {
+    lib: {
+      formats: ["es"],
+      entry: path.resolve(__dirname, "src/lib.ts"),
+      name: "visualia",
+      fileName: (format) => {
+        return {
+          es: "visualia.js",
+        }[format];
+      },
+    },
+    rollupOptions: {
+      external: ["vue"],
+      output: {
+        globals: {
+          vue: "Vue",
+        },
+      },
+    },
+  },
+  cjs: {
+    lib: {
+      formats: ["cjs", "umd"],
+      entry: path.resolve(__dirname, "src/lib.ts"),
+      name: "visualia",
+      fileName: (format) => {
+        return {
+          cjs: "visualia.cjs",
+          umd: "visualia.umd.js",
+        }[format];
+      },
+    },
+    rollupOptions: {
+      external: ["vue"],
+      output: {
+        globals: {
+          vue: "Vue",
+        },
+      },
+    },
+  },
+};
+
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [vue({ reactivityTransform: true })],
+    build: build[mode],
+  };
 });
