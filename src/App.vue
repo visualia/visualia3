91d7d419df93f35c95e5766f4068a186a587b7a1
kristjanjansen <kristjan.jansen@gmail.com>
2021-12-22 08:10:02 +0200

    chor: cleanup

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <script setup lang="ts">
-import VTest from "./components/VTest.vue";
+import { watchEffect } from "vue";
+import { v } from "./lib";
+
 const x = $ref(10);
+watchEffect(() => (v.x = x));
 </script>
 
 <template>
   <v-test>Hello x: {{ x }}</v-test>
-  <input type="range" v-model="x" />
+  <v-slider v-model="x" />
+  <br />
+  <v-test>Hello v.x: {{ v.x }}</v-test>
+  <v-slider v-model="v.x" />
 </template>
--- src/lib.ts
+++ src/lib.ts
@@ -1,7 +1,9 @@
 import { Plugin, reactive } from "vue";
 import VTest from "./components/VTest.vue";
 import VSlider from "./components/VSlider.vue";
-const v = reactive({});
+
+type V = Record<string, string | number | boolean>;
+export const v = reactive<V>({});
 
 export const Visualia: Plugin = {
   install: (app) => {
