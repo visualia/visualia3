0a0b6a0ab586c13857fc454aa5cb3bf6b7c169ef
kristjanjansen <kristjan.jansen@gmail.com>
2021-12-28 21:05:46 +0200

    fix: correct path

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,4 @@
 export { default as VMath } from "./VMath.vue";
 export { default as VPdf } from "./VPdf.vue";
-export { default as VPdfRect } from "./VPdfRect.vue";
+export { default as VPdfPath } from "./VPdfPath.vue";
 export { default as VSlider } from "./VSlider.vue";
