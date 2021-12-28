e51941702cfd416ae82e76e0cc3caab5c4b4f3f9
kristjanjansen <kristjan.jansen@gmail.com>
2021-12-28 16:51:34 +0200

    chore: cleanup

--- src/utils/point.test.ts
+++ src/utils/point.test.ts
@@ -1,11 +1,10 @@
-import { test, expect } from "vitest";
+import { test, assert } from "vitest";
 import { rectpoints } from "./point";
 
-test("rectpoints 10,20,30,40", () => {
-  return expect(rectpoints(10, 20, 30, 40)).toMatchObject([
+test("rectpoints 10,20,30,40", () =>
+  assert.sameDeepOrderedMembers(rectpoints(10, 20, 30, 40), [
     { x: 10, y: 20 },
     { x: 40, y: 20 },
     { x: 40, y: 60 },
     { x: 10, y: 60 },
-  ]);
-});
+  ]));
