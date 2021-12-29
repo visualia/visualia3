c4f800c232ff9a3c64fde939ce76e776e5f93fd5
kristjanjansen <kristjan.jansen@gmail.com>
2021-12-29 15:52:45 +0200

    chore: hm

--- docs/components/v-pdf.md
+++ docs/components/v-pdf.md
@@ -2,7 +2,7 @@
 
 <v-canvas>
   <v-canvas-path :path="circlepath(v.x || 0, 20, 30)" :opacity="1" />
-  <v-canvas-path :path="linepath(rectpoints(20, 30, 30, 40), true)" :opacity="0.1" />
+  <v-canvas-path :path="linepath(rectpoints(20, 30, 30, 40), true)" :opacity="0.1" :rotate="20" />
 </v-canvas>
 
 <br />
--- src/components/VCanvasPath.vue
+++ src/components/VCanvasPath.vue
@@ -1,8 +1,15 @@
 <script setup lang="ts">
 import { inject, watch } from "vue";
+import svgpath from "svgpath";
+//import svgPathBounds from "svg-path-bounds";
+
 import type { Context } from "./VCanvas.vue";
 
-const { path = "", opacity = 1 } = defineProps(["path", "opacity"]);
+const {
+  path = "",
+  opacity = 1,
+  rotate = 0,
+} = defineProps(["path", "opacity", "rotate"]);
 
 const context = inject("context") as Context;
 
@@ -10,10 +17,17 @@ watch(
   [context.ctx, context.updated, () => path],
   () => {
     if (context.ctx?.value) {
-      const p = new Path2D(path);
+      // const bounds = svgPathBounds(path);
+      // console.log(bounds);
+      const rotatedPath = svgpath(path).rotate(rotate).toString();
+      const p = new Path2D(rotatedPath);
       context.ctx.value?.save();
       context.ctx.value.globalAlpha = opacity;
       context.ctx.value?.stroke(p);
+
+      const p2 = new Path2D(path);
+      context.ctx.value?.stroke(p2);
+
       context.ctx.value?.restore();
     }
   },
