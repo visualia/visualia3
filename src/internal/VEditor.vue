ab019d5cacb2685019ea50a779678e93667bbc04
kristjanjansen <kristjan.jansen@gmail.com>
2022-01-13 15:18:13 +0200

    cleanup

--- src/internal/VEditor.vue
+++ src/internal/VEditor.vue
@@ -1,17 +1,37 @@
 <script setup lang="ts">
-import { ref, computed } from "vue";
+import { ref, computed, onMounted } from "vue";
 import VCompiler from "./VCompiler.vue";
 import MarkdownIt from "markdown-it";
 
 const { content: inputContent } = defineProps(["content"]);
 const content = ref(inputContent);
-const md = new MarkdownIt();
+const md = new MarkdownIt({ linkify: true, html: true, breaks: true });
 const outputContent = computed(() => md.render(content.value));
+const editor = ref<HTMLTextAreaElement | null>(null);
+onMounted(() => {
+  if (editor?.value) {
+    editor.value.onkeydown = function (e) {
+      if (e.keyCode === 9) {
+        const val = this.value;
+        const start = this.selectionStart;
+        const end = this.selectionEnd;
+        this.value = val.substring(0, start) + "  " + val.substring(end);
+        this.selectionStart = this.selectionEnd = start + 2;
+        return false;
+      }
+    };
+  }
+});
 </script>
 <template>
-  <textarea
-    v-model="content"
-    class="font-mono whitespace-pre bg-gray-800 text-gray-100 p-4"
-  />
-  <VCompiler :content="outputContent" />
+  <div class="grid grid-cols-2 gap-4">
+    <textarea
+      ref="editor"
+      v-model="content"
+      class="w-full font-mono text-sm whitespace-pre bg-gray-800 text-gray-100 p-4"
+    />
+    <div class="p-4">
+      <VCompiler :content="outputContent" />
+    </div>
+  </div>
 </template>
