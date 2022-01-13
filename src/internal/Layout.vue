bed4ab651941ac52f81ffe70015baea9e99975ea
kristjanjansen <kristjan.jansen@gmail.com>
2022-01-13 12:54:18 +0200

    fix

--- src/internal/Layout.vue
+++ src/internal/Layout.vue
@@ -6,11 +6,9 @@ import Nav from "./Nav.vue";
 <template>
   <Header />
   <main
-    class="grid md:grid-cols-[200px_1fr] grid-cols-1 grid-rows-[30vh_1fr] min-h-screen"
+    class="grid md:grid-cols-[200px_1fr] grid-cols-1 grid-rows-[30vh_1fr] md:grid-rows-1 min-h-screen"
   >
-    <Nav
-      class="top-0 bottom-0 p-4 md:p-8 shadow-xl overflow-auto md:h-screen"
-    />
+    <Nav class="bottom-0 p-4 md:p-8 shadow-xl overflow-auto" />
     <article
       class="p-5 md:p-12 prose prose-headings:text-gray-600 prose-strong:font-medium prose-a:text-sky-600 prose-a:underline prose-a:underline-offset-2 prose-a:break-words prose-code:before:content-none prose-code:after:content-none prose-pre:bg-sky-100 prose-code:text-sky-700 prose-code:bg-sky-100 prose-code:font-normal prose-code:px-[0.2em] prose-code:py-[0.1em] prose-p:before:content-none prose-p:after:content-none prose-blockquote:border-l-4 prose-blockquote:border-yellow-400 prose-blockquote:not-italic prose-blockquote:font-normal prose-blockquote:pl-6"
     >
